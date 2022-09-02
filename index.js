01c40eb939361022aea7286bb6726be665a688fe
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-09-01 23:03:10 -0300

    Validações com joi implementadas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-import Joi from "joi";
+import joi from "joi";
 import dayjs from "dayjs";
 import dotenv from "dotenv";
 
@@ -19,13 +19,34 @@ mongoClient.connect().then(() => {
     db = mongoClient.db("apiuol");
 });
 
-const schema = Joi.object({
-    username: Joi.string().min(3).max(30).required(),
+const nameSchema = joi.object({
+    name: joi.string().trim().min(1).max(30).required().strict(),
 });
+
+const messageSchema = joi.object({
+    to: joi.string().trim().min(1).required(),
+    text: joi.string().trim().min(1).required(),
+    type: joi.string().equal("message").equal("private_message"),
+});
+
+const now = dayjs().format("HH:mm:ss");
 
 app.post("/participants", async (req, res) => {
     const participant = req.body.name;
-    const now = dayjs().format("HH:mm:ss");
+
+    const validation = nameSchema.validate(
+        {
+            name: participant,
+        },
+        { abortEarly: false }
+    );
+
+    if (validation.error) {
+        const erros = validation.error.details.map(
+            (details) => details.message
+        );
+        return res.status(422).send(erros);
+    }
 
     const alreadyExists = await db
         .collection("participants")
@@ -33,14 +54,6 @@ app.post("/participants", async (req, res) => {
 
     if (alreadyExists) {
         return res.status(409).send({ message: "Nome já existente" });
-    }
-
-    try {
-        const value = await schema.validateAsync({
-            username: participant,
-        });
-    } catch (err) {
-        return res.status(422).send({ message: "Nome irregular" });
     }
 
     try {
@@ -73,6 +86,29 @@ app.get("/participants", async (req, res) => {
     } catch (error) {
         res.sendStatus(500);
     }
+});
+
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
+
+    const validation = messageSchema.validate(
+        {
+            to,
+            text,
+            type,
+        },
+        { abortEarly: false }
+    );
+
+    if (validation.error) {
+        const erros = validation.error.details.map(
+            (details) => details.message
+        );
+        return res.status(422).send(erros);
+    }
+
+    res.send(text);
 });
 
 app.listen(5000, () => console.log("escutando na porta 5000"));
